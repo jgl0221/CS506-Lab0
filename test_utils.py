25e2cbc972c8f6e9d738fbd8614bff22d23f6630
jgl0221 <jlee022103@gmail.com>
2024-09-12 15:17:20 -0400

    completed test files

--- test_utils.py
+++ test_utils.py
@@ -13,19 +13,22 @@ def test_dot_product():
     assert result == 32, f"Expected 32, but got {result}"
     
 def test_cosine_similarity():
-    ### YOUR CODE HERE
+    vector1 = np.array([3, 3, 3])
+    vector2 = np.array([3, 3, 3])
     
-    result = ### YOUR CODE HERE
+    result = cosine_similarity(vector1, vector2)
     
-    expected_result = ### YOUR CODE HERE
+    expected_result = 1
     
     assert np.isclose(result, expected_result), f"Expected {expected_result}, but got {result}"
 
 def test_nearest_neighbor():
-    ### YOUR CODE HERE
+    target_vector = [1, 2, 3]
+    vectors = [[4, 9, 1], [6, 7, 2], [0, 0, 1], [1, 2, 3], [10, 0, 5]]
     
-    result = ### YOUR CODE HERE
+    result = nearest_neighbor(target_vector, vectors)
     
-    expected_index = ### YOUR CODE HERE
+    expected_index = 3
     
     assert result == expected_index, f"Expected index {expected_index}, but got {result}"
+
